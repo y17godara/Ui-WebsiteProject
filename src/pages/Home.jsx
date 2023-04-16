af4e10afa6f2f09b3945951a3b20e65aa18b9733
Yash Godara <yash17godara@gmail.com>
2023-04-17 04:58:54 +0530

    working bg_ image for mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,11 +12,13 @@ const Home = () => {
         <div className='bg-[#F5F5F5]' >
 
           <div class="h-full bg-hero_pattern bg_image_css">
-
-            <div className="text">
-              Hello
+{/*
+            <div class="hero-text">
+              <h1 style="font-size:50px">I am Jane Doe</h1>
+              <h3>And I'm a Photographer</h3>
+               <button>Hire me</button> 
             </div>
-
+*/}
           </div>
 
         </div>
