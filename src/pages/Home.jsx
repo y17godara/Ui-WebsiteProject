cfe1518be8b2e57313db9b5a0cf3b1f781dcfee5
Yash Godara <yash17godara@gmail.com>
2023-04-17 09:29:52 +0530

    store section updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../static/css/Home.css';
 import '../static/css/index.css';
+import storeLogo from '../static/assets/logoStore.svg';
 
 const Home = () => {
 
@@ -16,9 +17,9 @@ const Home = () => {
 
             <div class="flex align-center text-center flex-col justify-center items-center m-auto max-w-[1440px] pt-12 px-2">
               <h1 className='flex text-[4rem] text-[#1A1A1A] font-black align-center justify-center text-center items-center sm:text-[8rem]'>Modern furniture with a Twist</h1>
-              
+
               <p className='flex text-[2rem] mt-8 text-[#1A1A1A] font-semibold align-center justify-center text-center items-center sm:text-[3rem]'>Ready-made furniture build for your home</p>
-              
+
               <div className='flex mt-8 flex-col gap-4 justify-center items-center text-center align-center sm:flex-row '>
                 <button className="button px-4 py-6">Shop</button>
                 <button className="button-2 px-4 py-6">Discover</button>
@@ -32,7 +33,18 @@ const Home = () => {
           {/* stores section */}
           <div className='h-full'>
 
-            <div className='flex my-[60px] bg-[#1A1A1A] align-center text-center flex-col justify-center items-center m-auto max-w-[1440px] pt-12 px-2'>
+            <div className='flex my-[60px] align-center text-center flex-col justify-center items-center m-auto max-w-[1440px] pt-12 px-2'>
+
+              <h1 className='flex text-[4rem] text-[#1A1A1A] font-bold align-center justify-center text-center items-center sm:text-[5rem]'>Find us in your favorite stores</h1>
+
+              <p className='flex text-[1.5rem] mt-8 text-[#1A1A1A] align-center justify-center text-center items-center sm:text-[2rem]'>And many other top retailers</p>
+
+              {/* imgs */}
+              <div className="flex flex-col justify-between items-center p-4 gap-4 w-full">
+
+                {/* <storeLogo /> */}
+
+              </div>
 
             </div>
 
