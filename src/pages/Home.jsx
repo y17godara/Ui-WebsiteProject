a6e17af8fb1e9795ed7b1cd6aaeb6f3f434d7815
Yash Godara <yash17godara@gmail.com>
2023-04-17 06:17:22 +0530

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import svgBackground from '../static/assets/background.svg';
 import '../static/css/Home.css';
+import '../static/css/index.css';
 
 const Home = () => {
 
@@ -12,13 +12,16 @@ const Home = () => {
         <div className='bg-[#F5F5F5]' >
 
           <div class="h-full bg-hero_pattern bg_image_css">
-{/*
-            <div class="hero-text">
-              <h1 style="font-size:50px">I am Jane Doe</h1>
-              <h3>And I'm a Photographer</h3>
-               <button>Hire me</button> 
+
+            <div class="flex align-center text-center flex-col justify-center items-center m-auto max-w-[1440px] pt-12">
+              <h1 className='flex text-[8rem] text-[#1A1A1A] font-black align-center justify-center text-center items-center'>Modern furniture with a Twist</h1>
+              <p className='flex text-[3rem] text-[#1A1A1A] font-semibold align-center justify-center text-center items-center'>Ready-made furniture build</p>
+              <div className='flex flex-col gap-4 m-4 justify-center items-center text-center align-center sm:flex-row '>
+                <button className="button px-4 py-6">Shop</button>
+                <button className="button-2 px-4 py-6">Discover</button>
+              </div>
             </div>
-*/}
+
           </div>
 
         </div>
