3a6d5c3a5c9e73b39b1b24b73a12b6f9da33dcd8
Yash Godara <yash17godara@gmail.com>
2023-04-17 07:33:57 +0530

    hero-section & navbar
    css Navbar changes, hero-section completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,12 +13,12 @@ const Home = () => {
 
           <div class="h-full bg_image_css">
 
-            <div class="flex align-center text-center flex-col justify-center items-center m-auto max-w-[1440px] pt-12">
-              <h1 className='flex text-[7rem] text-[#1A1A1A] font-black align-center justify-center text-center items-center'>Modern furniture with a&nbsp;<span className='greeting'>Twist</span></h1>
+            <div class="flex align-center text-center flex-col justify-center items-center m-auto max-w-[1440px] pt-12 px-2">
+              <h1 className='flex text-[4rem] text-[#1A1A1A] font-black align-center justify-center text-center items-center sm:text-[8rem]'>Modern furniture with a Twist</h1>
               
-              <p className='flex text-[2.5rem] text-[#1A1A1A] font-semibold align-center justify-center text-center items-center'>Ready-made furniture build for your home</p>
+              <p className='flex text-[2rem] mt-8 text-[#1A1A1A] font-semibold align-center justify-center text-center items-center sm:text-[3rem]'>Ready-made furniture build for your home</p>
               
-              <div className='flex flex-col gap-4 m-4 justify-center items-center text-center align-center sm:flex-row '>
+              <div className='flex mt-8 flex-col gap-4 justify-center items-center text-center align-center sm:flex-row '>
                 <button className="button px-4 py-6">Shop</button>
                 <button className="button-2 px-4 py-6">Discover</button>
               </div>
